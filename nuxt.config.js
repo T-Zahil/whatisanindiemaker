eea9348355686596e33c762842bb2221d65a7f1a
Thomas Bnt <thomasbnt@protonmail.com>
2022-01-06 20:02:53 +0100

    Add name for PWA

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,8 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: 'en',
+      name: 'What is an Indie Maker'
     }
   },
 
