7eb555bdfae6a9fc6ef94cb006c70ec2dad0d27f
Thomas Sanlis <thomas.sanlis@gmail.com>
2022-01-14 11:14:44 +0100

    Add PH badge

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="w-full min-h-screen overflow-hidden bg-orange-100 max-w-screen">
     <Nuxt />
+    <a href="https://www.producthunt.com/posts/what-is-an-indie-maker?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-what-is-an-indie-maker" target="_blank"><img
+      src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=326576&theme=light"
+      alt="What is an Indie Maker? - Are you tired of explaining what an indie maker is? | Product Hunt"
+      style="width: 250px; height: 54px;"
+      width="250"
+      height="54"
+      class="fixed top-2 left-2"
+    ></a>
   </div>
 </template>
 
