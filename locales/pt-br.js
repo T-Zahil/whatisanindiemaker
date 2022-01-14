b4a0bbc9cd52e66bdfe2c9f99f0bf56f21955890
Cosme Faé <cosmefm@gmail.com>
2022-01-14 16:21:29 -0300

    pt-br.js
    Added brazilian Portuguese (lang).

---
+++ locales/pt-br.js
@@ -0,0 +1,59 @@
+export default {
+  hero: {
+    title: 'O que é um Indie Maker?',
+    subtitle: 'Indie Maker é um {span}. Eles constroem projetos com recursos próprios, muitas vezes sem investimento, cuidando de todas as áreas: desenvolvimento, design, marketing...',
+    independantCreator: 'criador independente'
+  },
+
+  msg: {
+    title: 'Se você quer entender melhor esta profissão, separa 5 minutos aqui!'
+  },
+
+  why: {
+    title: 'Por quê?',
+    content: [
+      'Porque é uma profissão nova, que muita gente ainda não conhece. Como muitos trabalhos freelance, pode se sentir um pouco em isolamento, que agravada se as pessoas à volta não entenderem o que você faz.',
+      'Além de trabalhar em um ambiente relativamente novo e técnico (Internet!!), os Indie Makers têm vários empregos ao mesmo tempo: desenvolvedor, designer, marketing, redator, gerente de mídia social...',
+      'E o mais difícil pra alguns parentes entenderem é que muitos deles não ganham a vida assim! E os sortudos passaram meses ou anos aprendendo e construindo projetos.',
+      'Este site tem como objetivo promover a profissão Indie Maker, explicando ao público em geral. Apenas lendo estas poucas linhas, você já está prestando um grande serviço aos milhares de Indie Makers que se esforçam todos os dias para criar os melhores serviços na Internet.'
+    ]
+  },
+
+  roles: {
+    title: 'O que um Indie Maker faz?',
+    developmentTitle: 'Desenvolvimento',
+    developmentContent: 'Sem necessariamente ser um especialista em desenvolvimento, um Indie Maker muitas vezes desenvolve seus próprios projetos.',
+    designTitle: 'Design',
+    designContent: 'Sem necessariamente ser um especialista em design, um Indie Maker muitas vezes cria seus próprios projetos.',
+    marketingTitle: 'Marketing',
+    marketingContent: 'O marketing é uma parte muito importante do trabalho deles: para vender seus projetos, eles precisam torná-los conhecidos!',
+    himselfTitle: 'Fazem tudo sozinho',
+    himselfContent: 'Um Indie Maker usam ferramentas que não precisam codificar, gerenciadores de conteúdo, modelos... Quanto mais tempo eles economizarem, melhor.',
+    smTitle: 'Mídias sociais',
+    smContent: 'O Twitter é o mais usado, mas eles conseguem usar vários: Youtube, Reddit, Instagram...',
+    seoTitle: 'SEO',
+    seoContent: 'Eles têm que fazer seus projetos se destacarem nos mecanismos de pesquisa!'
+  },
+
+  vocabulary: {
+    title: 'Vocabulário básico',
+    mrr: 'MRR / ARR',
+    mrrDef: 'Receita recorrente mensal (Monthly Recurring Revenue) / Receita recorrente anual (Annual Recurring Revenue). O que um Indie Maker ganha a cada mês/ano em receita recorrente.',
+    ramenProfitability: 'Lucratividade Ramen',
+    ramenProfitabilityDef: 'Ganhando apenas o suficiente para viver.',
+    saas: 'SAAS',
+    saasDef: 'Software Como Um Serviço (Software As A Service). Um software online, geralmente vendido com uma assinatura. Permite ter receitas recorrentes.',
+    churn: 'Churn',
+    churnDef: 'O número de pessoas que cancelaram a assinatura do projeto.',
+    mvp: 'MVP',
+    mvpDef: 'Produto minimamente viável (Minimum Viable Product). A versão mais minimalista do seu produto.',
+    kpi: 'KPI',
+    kpiDef: 'Indicador-Chave de Desempenho (Key Performance Indicator). Indicadores numéricos que permitem medir um desempenho. Ex: número de visitas diárias.'
+  },
+
+  languages: {
+    fr: 'Français',
+    en: 'English',
+    pt: 'Portuguese'
+  }
+}
