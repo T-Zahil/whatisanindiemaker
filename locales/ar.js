8b15bdc165a1dccd84795710adf0fd697d2803db
mlaarebi <50870076+mlaarebi@users.noreply.github.com>
2022-04-18 05:02:39 +0000

    Create ar.js

---
+++ locales/ar.js
@@ -0,0 +1,59 @@
+export default {
+  languageName: 'العربية',
+
+  hero: {
+    title: 'What is an Indie Maker?',
+    subtitle: 'An indie maker is an {span}. They build projects with their own resources, often without funding, taking care of all aspects: development, design, marketing...',
+    independantCreator: 'صانع مستقل',
+  },
+
+  msg: {
+    title: '    إن أردت فهم هذه المهنة جيدا، استثمر 5 دقائق في قراءة ما يلي',
+  },
+
+  why: {
+    title: 'لماذا؟',
+    content: [
+      "Because it is a new profession, which many people do not know yet. Like many freelance jobs, it can lead to feelings of isolation, which are compounded if loved ones don't understand what you do.",
+      'In addition to working in a relatively new and technical environment (internet!!), indie makers have several jobs at the same time: developer, designer, marketer, writer, social media manager...',
+      "And the hardest thing for relatives to understand is that many of them don't make a living that way! And those who are lucky enough have spent months or years learning and building projects.",
+      'This website aims to promote the indie maker profession by explaining it to the general public. Just by reading these few lines, you are already doing a great service to the thousands of indie makers who strive every day to create the best services on the Internet.',
+    ],
+  },
+
+  roles: {
+    title: 'What does an indie maker do?',
+    developmentTitle: 'Development',
+    developmentContent: 'Without necessarily being an expert in development, an indie maker very often develop their own projects.',
+    designTitle: 'Design',
+    designContent: 'Without necessarily being an expert in design, an indie maker very often design their own projects.',
+    marketingTitle: 'Marketing',
+    marketingContent: 'Marketing is a very important part of their work: to sell your projects, they have to make them known!',
+    himselfTitle: 'Do everything themselves',
+    himselfContent: 'An indie maker can very well use no-code tools, CMS, templates... The more time they saves the better.',
+    smTitle: 'Social media',
+    smContent: 'Twitter is the best known, but they can use all of them: youtube, reddit, instagram...',
+    seoTitle: 'SEO',
+    seoContent: 'They have to make their projects stand out on search engines themselves!',
+  },
+
+  vocabulary: {
+    title: 'Some vocabulary',
+    mrr: 'MRR / ARR',
+    mrrDef: 'Monthly / Annual Recurring Revenue. What an indie maker earns each month / year in recurring revenue.',
+    ramenProfitability: 'Ramen profitability',
+    ramenProfitabilityDef: 'Earning just enough to live on.',
+    saas: 'SAAS',
+    saasDef: 'Software As A Service. An online software, often sold with a subscription. It allows to have recurring revenues.',
+    churn: 'Churn',
+    churnDef: 'The number of people who unsubscribe from your project.',
+    mvp: 'MVP',
+    mvpDef: 'Minimum Viable Product. The most minimalist version of your product.',
+    kpi: 'KPI',
+    kpiDef: 'Key Performance Indicator. Numerical indicators that allow to measure a performance. Ex: number of daily visits.',
+  },
+
+  contributors: {
+    title: 'Contributors',
+  },
+}
