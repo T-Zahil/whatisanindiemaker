cc312a8fa3a6f4dd3ee3ea12d73785d2e5e3351d
Ivan Klimchuk <alroniks@gmail.com>
2022-11-07 17:02:30 +0300

    Add Belarusian language

--- locales/index.js
+++ locales/index.js
@@ -1,1 +1,1 @@
-export default ['de', 'en', 'fr', 'it', 'ru', 'pt-br', 'ara', 'sp', 'tr']
+export default ['be', 'de', 'en', 'fr', 'it', 'ru', 'pt-br', 'ara', 'sp', 'tr']
--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ export default {
   ],
 
   i18n: {
-    locales: ['de', 'en', 'fr', 'it', 'ru', 'pt-br', 'ara', 'sp', 'tr'],
+    locales: ['be', 'de', 'en', 'fr', 'it', 'ru', 'pt-br', 'ara', 'sp', 'tr'],
     defaultLocale: 'en',
     vueI18n: {
       fallbackLocale: 'en',
