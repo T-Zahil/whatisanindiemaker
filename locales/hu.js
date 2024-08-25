c585e28a6069d80552642294b5833f457be72d73
Richard Szilagyi <szprichard@proton.me>
2024-08-25 07:03:56 +0100

    chore(i18n): Add Hungarian translation

---
+++ locales/hu.js
@@ -0,0 +1,67 @@
+export default {
+	languageName: "English",
+
+	hero: {
+		title: "Mi az az Indie Készítő?",
+		subtitle:
+			"Az indie készítő az egy független alkotó. Saját forrásból építenek projekteket, gyakran finanszírozás nélkül, mindenről gondoskodva: fejlesztésről, tervezésről, marketingről...",
+	},
+
+	msg: {
+		title: "Ha szeretnéd jobban megérteni ezt a szakmát, szánj rá 5 percet és olvass tovább!",
+	},
+
+	why: {
+		title: "Miért?",
+		content: [
+			"Mivel ez egy új karrierút, amiről még nem sokan tudnak. Sok szabadúszó karrierhez hasonlóan ez is az elszigeteltség érzéséhez vezethet, amely fokozódik, ha a körülötted lévők nem értik, mit csinálsz.",
+			"Amellett, hogy egy viszonylag új és technikai környezetben (az interneten!!) dolgoznak, az indie készítőknek több munkájuk is van egyszerre: fejlesztők, tervezők, marketingesek, írók, közösségi média menedzserek.",
+			"A család és a rokonok számára a legnehezebb megérteni, hogy sokan közülük nem is élnek meg ebből! Akik pedig szerencsések hónapokat vagy éveket töltöttek tanulással és projektek építésével.",
+			"Ennek a weboldalnak az a célja, hogy megpróbálja népszerűsíteni az indie készítői szakmát azáltal, hogy elmagyarázza azt a nyilvánosságnak. E néhány sor elolvasásával máris több ezer alkotónak segítesz, akik nap mint nap arra törekednek, hogy a legjobb szolgáltatásokat hozzák létre az interneten.",
+		],
+	},
+
+	roles: {
+		title: "Mit csinál egy indie készítő?",
+		developmentTitle: "Fejlesztés",
+		developmentContent:
+			"Anélkül, hogy szakértők lennének a szoftverprogramozásban, az indie készítők gyakran maguk fejlesztik projekteiket.",
+		designTitle: "Tervezés",
+		designContent:
+			"Anélkül, hogy feltétlenül szakértő lenne a tervezésben, egy indie készítő gyakran saját projektjeit tervezi.",
+		marketingTitle: "Marketing",
+		marketingContent:
+			"A marketing nagyon fontos része a munkának: ahhoz, hogy eladhassák projekteiket, ismertté kell tenni őket!",
+		himselfTitle: "Mindent maguk csinálnak",
+		himselfContent:
+			"Egy indie készítő nagyon jól tud olyan segédeszközöket használni mint a no-code, CMS és sablonok... Minél több időt takarítanak meg, annál jobb.",
+		smTitle: "Közösségi média",
+		smContent:
+			"A Twitter a legismertebb, de bármilyen platformot használhatnak: YouTube, Reddit, Instagram...",
+		seoTitle: "SEO",
+		seoContent:
+			"A keresőoptimalizálás biztosítja a projektek megtalálását!",
+	},
+
+	vocabulary: {
+		title: "Egy kis szókincs",
+		mrr: "MRR / ARR",
+		mrrDef: "Havi / éves ismétlődő bevétel. Amit egy indie készítő havonta/évente visszatérő bevételként keres.",
+		ramenProfitability: "Ramen nyereségesség",
+		ramenProfitabilityDef:
+			"Annyi nyereséget termelni, amiből megengedhesse magának az olcsó instant tésztát (ramen).",
+		saas: "SAAS",
+		saasDef:
+			"Software As A Service. Online szoftver, amelyet gyakran előfizetéssel árusítanak. Lehetővé teszi az ismétlődő bevételeket.",
+		churn: "Churn",
+		churnDef: "A szolgáltatáshoz való hozzáférést lemondó ügyfelek száma.",
+		mvp: "MVP",
+		mvpDef: "Minimum Viable Product. A termék legminimálisabb változata, ami a felhasználók számára használható.",
+		kpi: "KPI",
+		kpiDef: "Key Performance Indicator. Számszerű mutatók, amelyek lehetővé teszik a teljesítmény mérését. Pl.: napi látogatások száma.",
+	},
+
+	contributors: {
+		title: "Közreműködők",
+	},
+};
